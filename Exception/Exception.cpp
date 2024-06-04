f50c58f54c8a89ddfa911e9c6d06f5e25011af9a
Mihaly24 <fairuzk20@gmail.com>
2024-06-04 18:25:59 +0700

    Menambahkan isi dan menyelesaian projek

--- Exception/Exception.cpp
+++ Exception/Exception.cpp
@@ -1,9 +1,18 @@
 #include <iostream>
 #include <exception>
 #include <array>
-using namespace std
+using namespace std;
 
 int main()
 {
-    std::cout << "Hello World!\n";
+    cout << "Awal Program" << endl;
+    try {
+        array<int, 3> data = { 6, 7, 8 };
+        cout << data.at(5) << endl;
+    }
+    catch (exception& e) {
+        cout << e.what() << endl;
+    }
+    cout << "Baris program yang terakhir" << endl;
+    return 0;
 }
