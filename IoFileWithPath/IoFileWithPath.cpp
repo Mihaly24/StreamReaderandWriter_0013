fe657ec9747a4d1537fa77bfb43a743d4b35b34e
Mihaly24 <fairuzk20@gmail.com>
2024-06-04 17:50:38 +0700

    Menambahkan sisanya dan menyelesaikan projek

--- IoFileWithPath/IoFileWithPath.cpp
+++ IoFileWithPath/IoFileWithPath.cpp
@@ -8,5 +8,36 @@ int main()
     string baris;
     string NamaFile;
 
+    cout << "Masukkan Nama File : ";
+    cin >> NamaFile;
 
+    ofstream outfile;
+    outfile.open(NamaFile + ".txt", ios::out);
+
+    cout << ">= Menulis file, \'q\' untuk keluar" << endl;
+
+    while (true) {
+        cout << "- ";
+        getline(cin, baris);
+        if (baris == "q") break;
+        outfile << baris << endl;
+    }
+    outfile.close();
+
+    ifstream infile;
+
+    infile.open(NamaFile + ".txt", ios::out);
+
+    cout << endl << ">= Membuka dan membaca file " << endl;
+    //jika tidak ada file maka
+    if (infile.is_open())
+    {
+        while (getline(infile, baris))
+        {
+            cout << baris << '\n';
+        }
+        infile.close();
+    }
+    else cout << "Unable to open file";
+    return 0;
 }
