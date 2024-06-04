d61d4d880c99fc076b6592c2cd5141967478fa4d
Mihaly24 <fairuzk20@gmail.com>
2024-06-04 17:35:35 +0700

    Menambahkan jika file tidak ada

--- IoFile/IoFile.cpp
+++ IoFile/IoFile.cpp
@@ -25,4 +25,13 @@ int main()
     infile.open("contohfile.txt");
 
     cout << endl << ">= Membuka dan membaca file " << endl;
+    //jika tidak ada file maka
+    if (infile.is_open())
+    {
+        while (getline(infile, baris))
+        {
+            cout << baris << '\n';
+        }
+        infile.close();
+    }
 }
